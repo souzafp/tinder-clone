9df8cb8b9f749819a9b0ebc08cb2064d738a7520
Filipe Souza <filipe10souza@gmail.com>
2021-06-13 23:27:49 -0300

    commit

---
+++ tinder-clone/CombineCardView.swift
@@ -0,0 +1,21 @@
+//
+//  CombineCardView.swift
+//  tinder-clone
+//
+//  Created by Filipe Souza on 13/06/21.
+//
+
+import UIKit
+
+class CombineCardView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .purple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
--- tinder-clone/CombineViewController.swift
+++ tinder-clone/CombineViewController.swift
@@ -11,14 +11,49 @@ class CombineViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = UIColor.systemGroupedBackground
         
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        self.adicionarCards()
+    }
+}
+
+extension CombineViewController{
+    func adicionarCards() {
+    
+        for pilhaCards in 1...3{
+            let redView = CombineCardView()
+            redView.frame = CGRect(x: 0, y: 0, width: view.bounds.width - 32, height: view.bounds.height * 0.7)
+            redView.center = view.center
+            
+            
+            let gesture = UIPanGestureRecognizer()
+            gesture.addTarget(self, action: #selector(handlerCard))
+            redView.addGestureRecognizer(gesture)
+            view.addSubview(redView)
+        }
         
-        redView.center = view.center
-        view.addSubview(redView)
+
+    }
+}
+
+extension CombineViewController {
+    @objc func handlerCard (_ gesture : UIPanGestureRecognizer){
+        if let card = gesture.view {
+            let point = gesture.translation(in: view)
+            
+            card.center = CGPoint(x: view.center.x + point.x , y: view.center.y + point.y)
+            
+            let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
+//            Voltar card ao centro
+            if gesture.state == .ended{
+                UIView.animate(withDuration: 0.2){
+                    card.center = self.view.center
+                    card.transform = .identity
+                }
+            }
+        }
     }
 }
